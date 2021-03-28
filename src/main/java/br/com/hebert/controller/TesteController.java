d3ebff007147791a9528447307596de2e1c3596c
Hebert Ferreira <hebertalvesferreira@gmail.com>
2021-03-28 00:56:32 -0300

    integracao aws s3

--- src/main/java/br/com/hebert/controller/TesteController.java
+++ src/main/java/br/com/hebert/controller/TesteController.java
@@ -1,28 +1,56 @@
 package br.com.hebert.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.hebert.dto.UsuarioDto;
+import com.amazonaws.services.s3.model.Bucket;
+
+import br.com.hebert.services.DocumentosService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @RestController()
-@RequestMapping(path = "/teste")
-@Api(value = "Teste", description = "Api de gerenciamento de anotações")
+@RequestMapping(path = "/aws")
+@Api(value = "Teste", description = "Api de gerenciamento de arquivos")
 public class TesteController {
 
-	@ApiOperation(value = "Busca informações do usuário")
-	@GetMapping(value = "/usuario", produces = MediaType.APPLICATION_JSON_VALUE)
-	public UsuarioDto getTestData() {
-		UsuarioDto usuario = new UsuarioDto();
-		usuario.setApelido("Helth");
-		usuario.setCep("32223-080");
-		usuario.setIdade(31);
-		usuario.setIdentificador("c927553");
-		usuario.setNomeCompleto("Hebert Alves Ferreira");
-		return usuario;
+	@Autowired
+	private DocumentosService documentosService;
+
+	@ApiOperation(value = "Lista Buckets")
+	@GetMapping(value = "/listarArmazenamentos", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Bucket> listarBuckets() {
+		return this.documentosService.listarBuckets();
+	}
+
+	@ApiOperation(value = "Criar armazenamento")
+	@PostMapping(value = "/criarArmazenamento", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity criarBucket(String nomeArmazenamento) {
+		Bucket bucket = this.documentosService.criarBucket(nomeArmazenamento);
+		if (bucket != null) {
+			return new ResponseEntity(bucket, HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
+	}
+
+	@ApiOperation(value = "Exclui armazenamento")
+	@ApiParam(value = "The user id", required = true)
+	@DeleteMapping(value = "/excluirBucket/{id}")
+	public ResponseEntity excluirBucket(@PathVariable String id) {
+		if (this.documentosService.excluirBucket(id)) {
+			return new ResponseEntity(HttpStatus.OK);
+		}
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
 }
---
+++ src/main/java/br/com/hebert/services/DocumentosService.java
@@ -0,0 +1,59 @@
+package br.com.hebert.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+
+@Service
+public class DocumentosService {
+
+	private static final String KEY = "AKIA2NLFPRXHA3CPRGFW";
+	private static final String SECRET_KEY = "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";
+
+	/**
+	 * Cria uma instância do client S3 da AWS
+	 * 
+	 * @return instância do tipo AmazonS3
+	 */
+	private AmazonS3 logginAWS() {
+		AWSCredentials credentials = new BasicAWSCredentials(this.KEY, this.SECRET_KEY);
+
+		AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.SA_EAST_1).build();
+		return s3client;
+	}
+
+	public Bucket criarBucket(String nomeBucket) {
+		AmazonS3 s3client = logginAWS();
+
+		if (s3client.doesBucketExist(nomeBucket)) {
+			return null;
+		}
+
+		return s3client.createBucket(nomeBucket);
+	}
+
+	public boolean excluirBucket(String nomeBucket) {
+		AmazonS3 s3client = logginAWS();
+		if (s3client.doesBucketExist(nomeBucket)) {
+			s3client.deleteBucket(nomeBucket);
+			return true;
+		}
+		return false;
+	}
+
+	public List<Bucket> listarBuckets() {
+		AmazonS3 s3Client = logginAWS();
+		return s3Client.listBuckets();
+	}
+
+}
